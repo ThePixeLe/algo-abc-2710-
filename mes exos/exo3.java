0b07b5bdd802ab87a4f2264a06379ece33214dbb
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-08 08:52:13 +0100

    exo 3 done correction dossier entrainement

--- mes exos/exo3.java
+++ mes exos/exo3.java
@@ -18,4 +18,49 @@
  * 
 */
 
+import java.util.Scanner; /* Importation de la bibliothèque Java Scanner */
 
+public class exo3 { /* Nom de la classe */
+
+    public static void main(String[] args) { /* Toujours le mettre pour tester votre code */
+
+        /***--------  Début du code  -------------------****/
+        Scanner sc = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner
+        int a;
+        int b;
+        int resultat;
+
+        // Lire les valeurs de a et b
+
+        System.out.print("a = ");
+        a = sc.nextInt();
+
+
+        System.out.print("b = ");
+        b = sc.nextInt();
+
+        System.out.println();
+
+        // Calculer le résultat de l'addition
+
+        resultat = a + b;
+
+        // Afficher le résultat de l'addition
+
+        System.out.println("resultat = a + b");
+        System.out.println();
+        System.out.println("resultat = " + resultat);
+
+        // Vérifier si le résultat est pair ou impair
+
+        if (resultat % 2 == 0) {
+            System.out.println ("Le nombre est pair");
+        } else {
+            System.out.print("Le nombre est impair");
+        }
+
+        sc.close();
+
+
+    }
+}
