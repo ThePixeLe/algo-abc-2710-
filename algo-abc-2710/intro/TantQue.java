c69f723a2cdc58a4f17388cf807e9261577b1c06
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-09 11:19:46 +0100

    ajout fichier Tanque dans intro

---
+++ algo-abc-2710/intro/TantQue.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TantQue {
+
+    public static void main(String[] args) {
+
+         int note;
+         Scanner sc = new Scanner(System.in); //* Initialsiation du scanner */
+
+         do { 
+
+            System.out.println("Veuillez vous attribuer une note entre 0 et 20");
+            note = sc.nextInt();
+             
+         } while(note>20 || note<0); // Do While répètera l'instruction tant que la condition est vrai
+
+
+                     System.out.println("Vous avez :"+note+"/20");
+                     
+                     
+                     sc.close(); // Fermeture du Scanner
+        
+    }
+    
+}
+// test 
