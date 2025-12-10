987c8f6ce4b53bb8744efb21c80ee508608c771d
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-10 15:40:56 +0100

    exo7 pas terminer

--- mes exos/exo7.java
+++ mes exos/exo7.java
@@ -16,3 +16,46 @@
  * 
 */
 
+import java.util.Scanner;     /*Importation de la bibliothèque Java Scanner */
+
+public class exo7 {    /* Nom de la classe */
+      
+    public static void main (String [] args ) {    /*Toujours le mettre pour tester votre code */
+
+     /***--------  Début du code  -------------------****/
+
+      Scanner sc = new Scanner(System.in);      // Déclaration du scanner Instance de l'Objet Scanner
+
+     boolean y;
+     boolean n;
+     String reponse;
+
+         // Lire les variables
+
+         System.out.print("y =");
+         y = sc.nextBoolean();
+
+
+         System.out.print("n");
+         n = sc.nextBoolean();
+
+         System.out.print("Tu n as pas repondu a la question");
+         reponse = sc.nextLine();
+
+     
+
+       // Vérifier si le résultat de la question
+
+       if (y) {
+        System.out.println("Nice to meet you");
+       } else if (n) {
+        System.out.println("So learn english !");
+       } else (reponse) {
+        System.out.println("Tu n as pas repondu a la question");
+       }
+
+
+
+        sc.close();// Fermeture du Scanner
+    }
+}
