681871aeccf977c4bf04bbfc8f2ce282c1649407
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-16 14:35:37 +0100

    creation du code pour calculer la surface

--- mes exo 2/surface/src/exo_surface.java
+++ mes exo 2/surface/src/exo_surface.java
@@ -6,7 +6,20 @@ public class exo_surface {
          
         Scanner sc = new Scanner(System.in);
 
+         double PI = Math.PI;
+         double rayon;
+         double angle;
 
+         System.out.println("==================================");
+         System.out.println("CALCUL DE LA SURFACE D'UN SECTEUR CIRCULAIRE");
+         System.out.println("Formule : Surface = Math.PI*Math.pow(rayon,:2)*angle/360");
+         System.out.println("==================================");
+
+
+         System.out.print("Entrez le rayon : ");
+         rayon = sc.nextDouble();
+
+         
 
 
          sc.close();
