ccd9214ecc2d9b2bf062e2e34a2b7765be0156cc
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-16 14:19:40 +0100

    mise a jour dossier exo volume et surface

--- mes exo 2/surface/src/exo_surface.java
+++ mes exo 2/surface/src/exo_surface.java
@@ -1,5 +1,14 @@
-public class App {
+import java.util.Scanner; 
+
+
+public class exo_surface {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+         sc.close();
     }
 }
