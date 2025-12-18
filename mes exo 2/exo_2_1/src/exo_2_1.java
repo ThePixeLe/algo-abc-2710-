f300eff1a224a6e96b5d4e6435d411df17c3dc7a
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-18 14:46:30 +0100

    exo 2.1 comparaison de nombre done

--- mes exo 2/exo_2_1/src/exo_2_1.java
+++ mes exo 2/exo_2_1/src/exo_2_1.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.*;
+
+public class exo_2_1 {
+    public static void main(String [] args) throws Exception {
+     
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Comparaison de nombres");
+
+        System.out.println("Quel est votre age ?");
+        int age = sc.nextInt();
+
+        if (age < 0) {
+            System.out.println("Vous n'etes pas encore né");
+        } else if ( age < 18)
+            System.out.println("Vous etes mineur");
+            else { 
+                System.out.println("Vous etes majeur");
+
+            }
+
+
+        System.out.println("");
+
+        sc.close();
     }
+    
 }
