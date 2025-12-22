88a1b235b90e387db8c1e2c3b9bb844115ff4a8f
ThePixeLe <lsoilihi@arfp.asso.fr>
2025-12-22 15:13:57 +0100

    jalon

--- jalon_algo/src/exo_1.java
+++ jalon_algo/src/exo_1.java
@@ -8,26 +8,19 @@ public class exo_1 {
         System.out.println("Tri de nombre ");
 
         System.out.println("Entrez la premiere valeur ");
-        boolean a = sc.nextBoolean();
+        int a = sc.nextInt();
 
         System.out.println("Entrez la deuxieme  valeur ");
-         boolean b = sc.nextBoolean();
-
+        int b = sc.nextInt();
+        
         System.out.println("Entrez la troisieme  valeur ");
-        boolean c = sc.nextBoolean();
+        int c = sc.nextInt();
+        
+        int temporaire = a;
+        a = b;
+        b = a;
 
-        if ( a == true ) {
-            System.out.printBoolean( " votre premiere valeur est vrai",b);
-
-        } else if ( b == true) {
-              System.out.print( " votre deuxieme valeur est vrai",a);
-        } 
-        else if ( c == false) {
-             System.out.print( " votre troisiem valeur est fausse",c);
-        }
-          else {
-            System.out.println( " Resulta de sortie");
-          }
+        if (   a=b != c)
 
 
         sc.close();
